0cd5f93e029755283ec6c058e1009048c01ab5e3
luca-aussem <luca-aussem@t-online.de>
2024-11-06 16:36:46 +0100

    Added: Status bar for chatroom

--- app/src/main/java/com/example/chatterplay/chat/Chatroom.kt
+++ app/src/main/java/com/example/chatterplay/chat/Chatroom.kt
@@ -1,5 +1,6 @@
 package com.example.chatterplay.chat
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,12 +20,39 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 
 class Chatroom(private val modifier: Modifier) {
+    private var name = ""
+    private var users = ArrayList<String>()
     private val messages = ArrayList<ChatMessage>()
 
     init {
+        this.loadRoomData()
+        this.loadRoomMessages()
+    }
+
+    private fun loadRoomData() {
+        // !TODO! Connect to database and load config data
+        this.name = "Chatroom_1"
+        this.users.add("Luca")
+        this.users.add("Viktor")
+        this.users.add("Maaran")
+    }
+
+    private fun loadRoomMessages() {
+        // !TODO! Connect to database and load messages
+        this.addMessage(ChatMessage("Luca", "Hi!!!"))
+        this.addMessage(ChatMessage("Viktor", "Moin\nIch bin der Viktor!"))
+        this.addMessage(ChatMessage("Maaran", "Hallo."))
+        this.addMessage(ChatMessage("Luca", "Hi!!!"))
+        this.addMessage(ChatMessage("Viktor", "Moin\nIch bin der Viktor!"))
+        this.addMessage(ChatMessage("Maaran", "Hallo."))
         this.addMessage(ChatMessage("Luca", "Hi!!!"))
         this.addMessage(ChatMessage("Viktor", "Moin\nIch bin der Viktor!"))
         this.addMessage(ChatMessage("Maaran", "Hallo."))
@@ -38,24 +66,51 @@ class Chatroom(private val modifier: Modifier) {
     fun Render() {
         val messages = this.messages
         val modifier = this.modifier
+        val chatroomName = this.name
+        val memberCount = this.users.count()
+
         var textInput by remember { mutableStateOf("") }
 
         Box(modifier = Modifier.fillMaxHeight(1f)) {
-            Row(modifier = Modifier.fillMaxHeight(0.90f)) {
-                RenderMessages(messages, modifier)
-            }
-            Row(modifier = Modifier.fillMaxHeight(1f).padding(start = 10.dp, bottom = 50.dp), verticalAlignment = Alignment.Bottom) {
-                Column {
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(0.75f),
-                        value = textInput,
-                        onValueChange = { textInput = it },
-                        label = { Text("Nachricht eingeben...") }
-                    )
+            Column {
+                Row(modifier = modifier.zIndex(10f)) {
+                    Column(modifier = Modifier
+                        .background(Color(64, 127, 183))
+                        .padding(vertical = 3.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(1f),
+                            color =  Color.White,
+                            fontSize = 25.sp,
+                            textAlign = TextAlign.Center,
+                            text = chatroomName,
+                        )
+                        Text(
+                            modifier = Modifier.fillMaxWidth(1f),
+                            color = Color.White,
+                            fontSize = 13.sp,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.Center,
+                            text = "(${memberCount} Mitglieder)"
+                        )
+                    }
                 }
-                Column(modifier = Modifier.padding(start = 10.dp)) {
-                    Button(onClick = { sendMessage(textInput); textInput = "" }) {
-                        Text("Send")
+                Row(modifier = Modifier.fillMaxHeight(0.90f)) {
+                    RenderMessages(messages, modifier)
+                }
+                Row(modifier = Modifier.fillMaxHeight(1f).padding(start = 10.dp, bottom = 50.dp), verticalAlignment = Alignment.Bottom) {
+                    Column {
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(0.75f),
+                            value = textInput,
+                            onValueChange = { textInput = it },
+                            label = { Text("Nachricht eingeben...") }
+                        )
+                    }
+                    Column(modifier = Modifier.padding(start = 10.dp)) {
+                        Button(onClick = { sendMessage(textInput); textInput = "" }) {
+                            Text("Send")
+                        }
                     }
                 }
             }
