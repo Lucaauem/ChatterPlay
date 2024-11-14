de3606e919a94697e9e1d1d0d93a49a98fed4353
Lucaauem <luca-aussem@t-online.de>
2024-11-14 11:15:30 +0100

    Added: Navigation on homescreen

--- app/src/main/java/com/example/chatterplay/MainActivity.kt
+++ app/src/main/java/com/example/chatterplay/MainActivity.kt
@@ -5,11 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.chatterplay.chat.ChatList
 import com.example.chatterplay.ui.theme.ChatterPlayTheme
 import com.example.chatterplay.user.User
@@ -27,6 +36,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalLayoutApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,13 +44,39 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChatterPlayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(Modifier.padding(innerPadding)) {
-                        // Menu Menu //
+                    Box(Modifier
+                        .padding(innerPadding)
+                        .fillMaxWidth(1.0f)
+                    ) {
+                        FlowColumn(
+                            Modifier.fillMaxWidth(1.0f),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            NavigationButton("Chatten")
+                            NavigationButton("Spielen", false)
+                        }
                     }
                 }
             }
         }
+    }
 
+    @Composable
+    private fun NavigationButton(text: String, isEnabled: Boolean=true) {
+        Button(
+            enabled = isEnabled,
+            onClick = { TODO() },
+            modifier = Modifier.fillMaxWidth(0.65f)
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier.padding(vertical = 0.dp),
+                fontSize = 20.sp
+            )
+        }
+    }
+
+    private fun openChats() {
         startActivity(Intent(this, ChatList::class.java))
     }
 }
