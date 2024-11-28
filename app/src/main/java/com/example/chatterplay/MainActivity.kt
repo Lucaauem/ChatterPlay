652cd5f59dcae40366f33db24b0bf9f22ef73222
Viktor Veselinov <viktor.veselinov@wzl-iqs.rwth-aachen.de>
2024-11-28 10:30:15 +0100

    MainActivity "rollbacked"

--- app/src/main/java/com/example/chatterplay/MainActivity.kt
+++ app/src/main/java/com/example/chatterplay/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,13 +16,17 @@ import com.example.chatterplay.ui.theme.ChatterPlayTheme
 
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-        val fourconnect = FourConnect()
-            fourconnect.GameUI()
+            ChatterPlayTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
     }
 }
