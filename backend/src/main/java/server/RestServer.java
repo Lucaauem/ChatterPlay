aff7bff957a85443fb9b2f69e8e996523d6647b6
luca-aussem <luca-aussem@t-online.de>
2024-12-19 15:47:37 +0100

    Fixed: Communication with external devices

--- backend/src/main/java/server/RestServer.java
+++ backend/src/main/java/server/RestServer.java
@@ -23,7 +23,7 @@ public class RestServer extends Application {
 
     public static void main(String[] args) throws Exception {
         Component component = new Component();
-        component.getServers().add(Protocol.HTTP, REST_PORT);
+        component.getServers().add(Protocol.HTTP, "0.0.0.0", REST_PORT);
         component.getDefaultHost().attach(new RestServer());
         component.setLogService(new LogService(false));
         component.start();
