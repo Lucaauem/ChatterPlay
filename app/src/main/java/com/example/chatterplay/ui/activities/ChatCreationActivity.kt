af97679d54936a1caf16e49a9ff6f6b0dc7a6889
Luca.aue <luca-aussem@t-online.de>
2024-12-21 01:00:57 +0100

    Updated: UI

--- app/src/main/java/com/example/chatterplay/ui/activities/ChatCreationActivity.kt
+++ app/src/main/java/com/example/chatterplay/ui/activities/ChatCreationActivity.kt
@@ -1,6 +1,14 @@
 package com.example.chatterplay.ui.activities
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowColumn
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -9,27 +17,56 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.chatterplay.communication.RestService
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 
 class ChatCreationActivity : AppActivity() {
+    @OptIn(ExperimentalLayoutApi::class)
     @Composable
     override fun Render() {
         var textInput by remember { mutableStateOf("") }
 
-        TextField(
-            modifier = Modifier.fillMaxWidth(0.75f),
-            value = textInput,
-            onValueChange = { textInput = it },
-            label = { Text("Name des Chats") }
-        )
-        Button(
-            enabled = textInput.isNotEmpty(),
-            onClick = { createChatroom(textInput) }
+        FlowColumn(
+            horizontalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
         ) {
-            Text("Erstellen")
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                TextField(
+                    modifier = Modifier.fillMaxWidth(0.75f),
+                    value = textInput,
+                    onValueChange = { textInput = it },
+                    label = { Text("Name des Chats") }
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    enabled = textInput.isNotEmpty(),
+                    onClick = { createChatroom(textInput) }
+                ) {
+                    Text(
+                        text = "Erstellen",
+                        modifier = Modifier.padding(4.dp),
+                        fontSize = 24.sp
+                    )
+                }
+            }
         }
     }
 
